63d50b0734ed3d138485f899b6d9e193dbd34d7c
KiboOst <KiboOst@users.noreply.github.com>
2020-01-15 23:03:54 +0100

    more

--- desktop/js/jeerhasspy.js
+++ desktop/js/jeerhasspy.js
@@ -331,9 +331,9 @@ $('.bt_openScenario').off('click').on('click', function () {
 })
 
 $('.bt_logScenario').off('click').on('click', function () {
-  $('#md_modal').dialog({title: "{{Log d'exécution du scénario}}"});
-  $("#md_modal").load('index.php?v=d&modal=scenario.log.execution&scenario_id=' + $('select[data-l2key="callbackScenario"]').val()).dialog('open');
-});
+  $('#md_modal').dialog({title: "{{Log d'exécution du scénario}}"})
+  $("#md_modal").load('index.php?v=d&modal=scenario.log.execution&scenario_id=' + $('select[data-l2key="callbackScenario"]').val()).dialog('open')
+})
 
 $(function() {
   //filter empty strings and remove 'None' group:
--- desktop/modal/intents.summary.php
+++ desktop/modal/intents.summary.php
@@ -1,12 +1,12 @@
 <?php
 if (!isConnect('admin')) {
-    throw new Exception('{{401 - Accès non autorisé}}');
+	throw new Exception('{{401 - Accès non autorisé}}');
 }
 
 $scenarios = scenario::all();
 $_scenarios = array();
 foreach ($scenarios as $scenario){
-  	$_scenarios[$scenario->getId()] = $scenario->getHumanName();
+	$_scenarios[$scenario->getId()] = $scenario->getHumanName();
 }
 sendVarToJS('_scenarios', $_scenarios);
 
@@ -27,8 +27,9 @@ sendVarToJS('_scenarios', $_scenarios);
 			<th data-sorter="select" data-filter="false">{{Action}}</th>
 			<th data-sorter="checkbox" data-filter="false">{{Actif}}</th>
 			<th data-sorter="inputs" data-filter="false">{{Min.Conf}}</th>
-          	<th data-sorter="false" data-filter="false">{{Tags}}</th>
+			<th data-sorter="false" data-filter="false">{{Tags}}</th>
 			<th data-sorter="false" data-filter="true">{{UserTags}}</th>
+			<th data-sorter="false" data-filter="false">{{Actions}}</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -40,7 +41,7 @@ sendVarToJS('_scenarios', $_scenarios);
 initTableSorter()
 refreshIntentsSummary()
 var tableIntcSummary = $('#table_intentsSummary')
-tableIntcSummary[0].config.widgetOptions.resizable_widths = ['', '', '120px', '45px', '95px', '70px', '160px']
+tableIntcSummary[0].config.widgetOptions.resizable_widths = ['', '', '120px', '45px', '95px', '70px', '160px', '40px']
 tableIntcSummary.trigger('applyWidgets')
 tableIntcSummary.trigger('resizableReset')
 tableIntcSummary.trigger('sorton', [[[1,0]]])
@@ -63,74 +64,86 @@ function refreshIntentsSummary() {
 			$('#table_intentsSummary tbody').empty()
 			var table = []
 			for(var i in data){
-              	var intent = data[i]
-                if (intent.configuration.type != 'intent') continue
+				var intent = data[i]
+				if (intent.configuration.type != 'intent') continue
 				var tr = '<tr class="intent" data-id="' + intent.id + '">'
 				tr += '<td>'
-                tr += '<input class="eqLogicAttr hidden" data-l1key="id">'
-                tr += '<input class="eqLogicAttr hidden" data-l1key="configuration" data-l2key="type">'
+				tr += '<input class="eqLogicAttr hidden" data-l1key="id">'
+				tr += '<input class="eqLogicAttr hidden" data-l1key="configuration" data-l2key="type">'
 				tr += '<span class="eqLogicAttr" data-l1key="name"></span>'
 				tr += '</td>'
 
-                tr += '<td>'
-                if (intent.configuration.hasOwnProperty('callbackScenario') && intent.configuration.callbackScenario.hasOwnProperty('scenario')) {
-                  	tr += '<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="scenario">'
-                    tr += '<option value="-1">None</option>'
-                    Object.entries(_scenarios).forEach(([key, value]) => tr += '<option value="' + key + '">' + value + '</option>')
-                  	tr += '</select>'
-                }
+				tr += '<td>'
+				if (intent.configuration.hasOwnProperty('callbackScenario') && intent.configuration.callbackScenario.hasOwnProperty('scenario')) {
+					tr += '<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="scenario">'
+					tr += '<option value="-1">None</option>'
+					Object.entries(_scenarios).forEach(([key, value]) => tr += '<option value="' + key + '">' + value + '</option>')
+					tr += '</select>'
+				}
 				tr += '</td>'
 
-                tr += '<td>'
+				tr += '<td>'
 				tr += '<select class="eqLogicAttr form-control input-sm" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="action">'
-                tr += '<option value="start">{{Start}}</option>'
-                tr += '<option value="startsync">{{Start (sync)}}</option>'
-                tr += '<option value="stop">{{Stop}}</option>'
-                tr += '<option value="activate">{{Activer}}</option>'
-                tr += '<option value="deactivate">{{Désactiver}}</option>'
-                tr += '<option value="resetRepeatIfStatus">{{Remise à zero des SI}}</option>'
+				tr += '<option value="start">{{Start}}</option>'
+				tr += '<option value="startsync">{{Start (sync)}}</option>'
+				tr += '<option value="stop">{{Stop}}</option>'
+				tr += '<option value="activate">{{Activer}}</option>'
+				tr += '<option value="deactivate">{{Désactiver}}</option>'
+				tr += '<option value="resetRepeatIfStatus">{{Remise à zero des SI}}</option>'
 				tr += '</select>'
 				tr += '</td>'
 
-                tr += '<td>'
+				tr += '<td>'
 				tr += '<center><input type="checkbox" class="eqLogicAttr" data-label-text="{{Actif}}" data-l1key="isEnable"></center>'
 				tr += '</td>'
 
-                tr += '<td>'
+				tr += '<td>'
 				tr += ' <input style="width: 100%;" type="number" value="0" min="0" max="1" step="0.1" class="eqLogicAttr input-sm" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="minConfidence" />';
 				tr += '</td>'
 
-                tr += '<td>'
-                tr += '<button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown">'
-                  tr += '<i class="fas fa-tags"></i>&nbsp;&nbsp;&nbsp;<span class="caret"></span>'
-                tr += '</button>'
-                tr += '<ul class="dropdown-menu" role="menu" style="top:unset;left:unset;">'
-                  tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagIntent"/>&nbsp;intent</a></li>'
-                  tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagEntities"/>&nbsp;entities</a></li>'
-                  tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagSlots"/>&nbsp;slots</a></li>'
-                  tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagSiteId"/>&nbsp;siteId</a></li>'
-                  tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagQuery"/>&nbsp;query</a></li>'
-                  tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagConfidence"/>&nbsp;confidence</a></li>'
-                  tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagWakeword"/>&nbsp;wakeword</a></li>'
-                tr += '</ul>'
+				tr += '<td>'
+				tr += '<button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown">'
+					tr += '<i class="fas fa-tags"></i>&nbsp;&nbsp;&nbsp;<span class="caret"></span>'
+				tr += '</button>'
+				tr += '<ul class="dropdown-menu" role="menu" style="top:unset;left:unset;">'
+					tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagIntent"/>&nbsp;intent</a></li>'
+					tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagEntities"/>&nbsp;entities</a></li>'
+					tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagSlots"/>&nbsp;slots</a></li>'
+					tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagSiteId"/>&nbsp;siteId</a></li>'
+					tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagQuery"/>&nbsp;query</a></li>'
+					tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagConfidence"/>&nbsp;confidence</a></li>'
+					tr += '<li><a tabIndex="-1"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="isTagWakeword"/>&nbsp;wakeword</a></li>'
+				tr += '</ul>'
 
-                tr += '</td>'
+				tr += '</td>'
 
-                tr += '<td>'
+				tr += '<td>'
 				tr += ' <input style="width: 100%;" class="eqLogicAttr input-sm" data-l1key="configuration" data-l2key="callbackScenario" data-l3key="user_tags" />';
 				tr += '</td>'
 
-                var result = $(tr)
-                result.setValues(intent, '.eqLogicAttr')
+				tr += '<td>'
+				tr += '<a class="btn btn-sm btn-success bt_summaryLogScenario" target="_blank" title="{{Ouvrir le log du scénario.}}"><i class="far fa-file-alt"></i> {{Log}}</a>';
+				tr += '</td>'
+
+				var result = $(tr)
+				result.setValues(intent, '.eqLogicAttr')
 				table.push(result)
 			}
 			$('#table_intentsSummary tbody').append(table)
 			$("#table_intentsSummary").trigger("update")
 
-            $('.bt_summaryGotoScenario').off().on('click', function() {
+			$('.bt_summaryGotoScenario').off().on('click', function() {
 				var scId = $(this).data('scid')
-                var url = 'index.php?v=d&p=scenario&id='+scId
-    			window.open(url).focus()
+				var url = 'index.php?v=d&p=scenario&id='+scId
+				window.open(url).focus()
+			})
+
+			$('.bt_summaryLogScenario').off('click').on('click', function () {
+				var scId = $(this).closest('.intent').find('select[data-l3key="scenario"]').val()
+				$("#md_modal2").dialog({title: "{{Log d'exécution du scénario}}"})
+				$("#md_modal2").load('index.php?v=d&modal=scenario.log.execution&scenario_id=' + scId).dialog('open')
+				var modal = $('.ui-dialog[aria-describedby="md_modal2"]')
+				modal.css({top: modal.position().top - 20 + "px"})
 			})
 		}
 	})
@@ -138,7 +151,7 @@ function refreshIntentsSummary() {
 
 function saveIntentsSummary() {
 	var intents = $('#table_intentsSummary tbody .intent').getValues('.eqLogicAttr')
-    jeedom.eqLogic.save({
+	jeedom.eqLogic.save({
 		eqLogics : intents,
 		type: 'jeerhasspy',
 		error: function (error) {
